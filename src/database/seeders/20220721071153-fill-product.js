ff7e8dd3f838fa88e1b68883d1fc3a26268222c2
evelinmerlians@gmail.com <mms.evelin8@gmail.com>
2022-07-21 14:52:48 +0700

    seeder

--- src/database/seeders/20220721071153-fill-product.js
+++ src/database/seeders/20220721071153-fill-product.js
@@ -11,6 +11,57 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+     await queryInterface.bulkInsert("Products", [
+       {
+      namaProduk: "penggaris",
+      deskripsi: "penggaris-30cm",
+      stok: 10,
+      tersedia: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     }
+    ])
+
+    await queryInterface.bulkInsert("Products", [
+      {
+     namaProduk: "penghapus",
+     deskripsi: "penghapus-karet",
+     stok: 0,
+     tersedia: false,
+     createdAt: new Date(),
+     updatedAt: new Date()
+    }
+   ])
+   await queryInterface.bulkInsert("Products", [
+      {
+     namaProduk: "pensil-kayu",
+     deskripsi: "pensil-kayu-2B",
+     stok: 2,
+     tersedia: true,
+     createdAt: new Date(),
+     updatedAt: new Date()
+    }
+   ])
+   await queryInterface.bulkInsert("Products", [
+      {
+     namaProduk: "pensil-mekanik",
+     deskripsi: "pensil-mekanik-2B",
+     stok: 20,
+     tersedia: true,
+     createdAt: new Date(),
+     updatedAt: new Date()
+    }
+   ])
+   await queryInterface.bulkInsert("Products", [
+      {
+     namaProduk: "spidol",
+     deskripsi: "spidol-permanen",
+     stok: 0,
+     tersedia: false,
+     createdAt: new Date(),
+     updatedAt: new Date()
+    }
+   ])
   },
 
   async down (queryInterface, Sequelize) {
@@ -20,5 +71,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+     await queryInterface.bulkDelete('Products', null)
   }
 };
